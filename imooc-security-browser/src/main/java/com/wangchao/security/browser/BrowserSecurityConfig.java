b7d9b03f22cb0492e3588f210a4e421c9b2fe095
wangchao <244067166@qq.com>
2018-10-20 15:56:14 +0800

    1

---
+++ imooc-security-browser/src/main/java/com/wangchao/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.wangchao.security.browser;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/imooc-signIn.html")
+                .loginProcessingUrl("/authentication/form")
+//        http.httpBasic()
+                .and().authorizeRequests()
+                .antMatchers("/imooc-signIn.html").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable();
+    }
+
+
+}
